92ae83e2b48686e1f2b5139b0918f3ce4f282a9c
Gerson <grs@cesar.org.br>
2023-03-20 16:12:58 -0300

    Update spark dockerfile and fix minio path

--- images/spark/ingestion_to_bronze.py
+++ images/spark/ingestion_to_bronze.py
@@ -13,6 +13,22 @@ if __name__ == "__main__":
     spark = (
         SparkSession.builder.appName("ingestion-to-bronze")
         .config("spark.sql.warehouse.dir", abspath("spark-warehouse"))
+        .config("spark.hadoop.fs.s3a.endpoint", "http://10.108.233.87:9000")
+        .config("spark.hadoop.fs.s3a.access.key", "gerson")
+        .config("spark.hadoop.fs.s3a.secret.key", "rHasMuUFzGY0REI2H6RcdaJFNZp9b9ht")
+        .config("spark.hadoop.fs.s3a.path.style.access", True)
+        .config("spark.hadoop.fs.s3a.fast.upload", True)
+        .config("spark.hadoop.fs.s3a.connection.maximum", 100)
+        .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
+        .config(
+            "spark.delta.logStore.class",
+            "org.apache.spark.sql.delta.storage.S3SingleDriverLogStore",
+        )
+        .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
+        .config(
+            "spark.sql.catalog.spark_catalog",
+            "org.apache.spark.sql.delta.catalog.DeltaCatalog",
+        )
         .enableHiveSupport()
         .getOrCreate()
     )
@@ -25,9 +41,10 @@ if __name__ == "__main__":
 
     # set dynamic input file [hard-coded]
     # can be changed for input parameters [spark-submit]
-    get_users_file = "s3a://landing-zone/users/*.json"
-    get_business_file = "s3a://landing-zone/business/*.json"
-    get_reviews_file = "s3a://landing-zone/reviews/*.json"
+    get_users_file = "/app/landing/user/*.json"
+    get_subscription_file = "/app/landing/subscription/*.json"
+    get_credit_card_file = "/app/landing/credit_card/*.json"
+    get_movies_file = "/app/landing/movies/*.json"
 
     # read user data
     df_user = (
@@ -37,102 +54,47 @@ if __name__ == "__main__":
         .json(get_users_file)
     )
 
-    # read business data
-    df_business = (
+    # read subscription data
+    df_subscription = (
         spark.read.format("json")
         .option("inferSchema", "true")
         .option("header", "true")
-        .json(get_business_file)
+        .json(get_subscription_file)
     )
 
-    # read review data
-    df_review = (
+    # read credit card data
+    df_credit_card = (
         spark.read.format("json")
         .option("inferSchema", "true")
         .option("header", "true")
-        .json(get_reviews_file)
+        .json(get_credit_card_file)
     )
 
-    # get number of partitions
-    # [25], [16], [48]
-    print(df_user.rdd.getNumPartitions())
-    print(df_business.rdd.getNumPartitions())
-    print(df_review.rdd.getNumPartitions())
+    # read movies data
+    df_movies = (
+        spark.read.format("json")
+        .option("inferSchema", "true")
+        .option("header", "true")
+        .json(get_movies_file)
+    )
 
     # print schema of dataframe
     df_user.printSchema()
-    df_business.printSchema()
-    df_review.printSchema()
+    df_subscription.printSchema()
+    df_credit_card.printSchema()
+    df_movies.printSchema()
 
-    # display data
-    df_user.show()
-    df_business.show()
-    df_review.show()
-
-    # count rows
-    df_user.count()
-    df_business.count()
-    df_review.count()
-
-    # register df into sql engine
-    df_business.createOrReplaceTempView("business")
-    df_user.createOrReplaceTempView("user")
-    df_review.createOrReplaceTempView("review")
-
-    # sql join into a new [df]
-    df_join = spark.sql(
-        """
-        SELECT u.user_id,
-               u.name AS user,
-               u.average_stars AS user_avg_stars,
-               u.useful AS user_useful,
-               u.review_count AS user_review_count,
-               u.yelping_since,
-               b.business_id,
-               b.name AS business,
-               b.city,
-               b.state,
-               b.stars AS business_stars,
-               b.review_count AS business_review_count,
-               r.useful AS review_useful,
-               r.stars AS review_stars,
-               r.date AS date
-        FROM review AS r
-        INNER JOIN business AS b
-        ON r.business_id = b.business_id
-        INNER JOIN user AS u
-        ON u.user_id = r.user_id
-    """
-    )
-
-    # show & count df
-    df_join.explain()
-    df_join.count()
-
-    # show df
-    df_join.show()
-
-    # save dataframe into postgres
-    # yugabytedb database [ysql]
-    # k8s cluster ip for yugabytedb [intra-communication]
-    """
-    df_join \
-        .write \
-        .format("jdbc") \
-        .mode("overwrite") \
-        .option("url", "jdbc:postgresql://10.0.206.213:5433/owshq") \
-        .option("dbtable", "public.fact_reviews") \
-        .option("user", "yugabyte") \
-        .option("password", "yugabyte") \
-        .save()
-    """
-
-    # write into parquet file on curated zone
+    # write into parquet file on bronze zone
     # file to be available for virtualization engine
     # using minio as storage inside of [k8s]
-    df_join.write.format("parquet").mode("overwrite").save(
-        "s3a://curated-zone/fact_reviews/"
+    df_user.write.format("delta").mode("overwrite").save("s3a://bronze/user/")
+    df_subscription.write.format("delta").mode("overwrite").save(
+        "s3a://bronze/subscription/"
     )
+    df_credit_card.write.format("delta").mode("overwrite").save(
+        "s3a://bronze/credit_card/"
+    )
+    df_movies.write.format("delta").mode("overwrite").save("s3a://bronze/movies/")
 
     # stop session
     spark.stop()
